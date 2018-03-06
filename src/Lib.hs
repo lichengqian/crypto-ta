6123ca1fac88229a1af9d1bd54b8fb2055a16a10
za-liming <liming@zhongan.io>
2018-03-06 10:51:08 +0800

    chart works

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,25 +2,47 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
 module Lib
-    ( startApp
-    , app
-    , main
+    ( main
+    -- , startApp
+    -- , app
     ) where
 
 
-import           Data.Aeson
-import           Data.Aeson.TH
+-- import           Data.Aeson
+-- import           Data.Aeson.TH
 import           Diagrams.Backend.Canvas.CmdLine
 import           Diagrams.Prelude
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
 import           Universum
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Diagrams
+import Data.Time.LocalTime
 
-b1 = (square 20 :: Diagram B) # lw 0.002
+import Prices(prices,mkDate,filterPrices)
 
-main = mainWith (pad 1.1 b1)
+prices' :: [(LocalTime,Double,Double)]
+prices' = filterPrices prices (mkDate 1 1 2005) (mkDate 31 12 2006)
 
+chart = toRenderable $ do
+    layoutlr_title .= "Price History"
+    layoutlr_left_axis . laxis_override .= axisGridHide
+    layoutlr_right_axis . laxis_override .= axisGridHide
+    plotLeft (line "price 1" [[ (d,v) | (d,v,_) <- prices']])
+    plotLeft (line "price 1.1" [[ (d,v + 2) | (d,v,_) <- prices']])
+    plotRight (line "price 2" [[ (d,v) | (d,_,v) <- prices']])
+
+main = do
+  env <- defaultEnv vectorAlignmentFns 700 400
+  let (diagram :: Diagram B, _) = runBackendR env chart
+  mainWith diagram
+ 
+-- b1 = (square 20 :: Diagram B) # lw 0.002
+
+-- main = mainWith (pad 1.1 b1)
+
+{--#
 data User = User
   { userId        :: Int
   , userFirstName :: String
@@ -47,3 +69,4 @@ users :: [User]
 users = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
         ]
+--}
